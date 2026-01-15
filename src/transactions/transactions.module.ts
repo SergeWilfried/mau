61b76e368b114e0e848a1e68a21072bab549dcc5
Serge Wilfried <willytheigeek@gmail.com>
2026-01-15 12:42:13 +0000

    feat: fetch add transactions

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
+import { SupabaseModule } from '../supabase/supabase.module';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+  imports: [SupabaseModule, CommonModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
   exports: [TransactionsService],
