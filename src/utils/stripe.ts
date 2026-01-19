474edea00dd871d67958723913bef0d506c70587
Serge Wilfried <willytheigeek@gmail.com>
2026-01-19 20:47:23 +0000

    fix: update app icons

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,8 +2,7 @@ import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   httpClient: Stripe.createFetchHttpClient(),
-  // https://github.com/stripe/stripe-node#configuration
-  apiVersion: "2025-12-15.clover",
+  apiVersion: "2025-12-18.acacia",
   appInfo: {
     name: "jamana dex",
   },
